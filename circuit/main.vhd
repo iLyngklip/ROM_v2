98455a0472ed9b5c91845527f3781a5ae12fd8dd
Mathias Lyngklip Kjeldgaard <mathiaslyngklip@gmail.com>
2017-03-16 13:53:08 +0100

    Commit før stor ændring

--- circuit/main.vhd
+++ circuit/main.vhd
@@ -74,7 +74,7 @@ architecture Behavioral of main is
 	signal whereToRead : integer range 0 to 7 := 0;
 	
 	-- READ STUFF
-	signal bin_value : std_logic_vector (7 downto 0) := (others => '0');
+	signal bin_value : std_logic_vector (8 downto 0) := (others => '0');
 	
 	
 begin -- MAIN BEGIN
@@ -102,33 +102,59 @@ begin -- MAIN BEGIN
 -- READ STUFF ------------------- READ STUFF ----------------------------
 process  
       file file_pointer : text;
-		variable line_content : string(1 to 4);
+		variable line_content : string(1 to 2);
       variable line_num : line;
 		variable j : integer := 0;
-		variable char : character:='0';
+		variable char : character :='0';
 		variable i : integer := 0;
+		variable ret : std_logic_vector(line_content'length*16-1 downto 0);
+		variable tempStorage : std_logic_vector(8 to 1);
 		
    begin
 		--Open the file read.txt from the specified location for reading(READ_MODE).
-      file_open(file_pointer,"C:\ROM_v2\circuit\read.txt", READ_MODE);	  
+      file_open(file_pointer,"C:\ROM_v2\circuit\read_2.txt", READ_MODE);	  
       while not endfile(file_pointer) loop --till the end of file is reached continue.
 			readline (file_pointer,line_num);  --Read the whole line from the file
 			--Read the contents of the line from  the file into a variable.
 			READ (line_num,line_content);  
 			--For each character in the line convert it to binary value.
 			--And then store it in a signal named 'bin_value'.
-			for j in 1 to 4 loop		 
-				char := line_content(j);
-				if(char = '0') then
-					bin_value(4-j) <= '0';
-				elsif (char = '1') then 
-					bin_value(4-j) <= '1';
-				else 
-					bin_value(7) <= '1'; 
-				end if;	
-			end loop;	
-			--wait for 10 ns; --after reading each line wait for 10ns.
-			MyRom(i) <= bin_value;
+			
+			for u in line_content'range loop
+				char := line_content(u);
+				tempStorage := std_logic_vector(to_unsigned(character'pos(char),8));
+				for y in 8 to 1 loop
+					bin_value(y) <= tempStorage(y);
+				end loop;
+			end loop;
+			
+--			for i in line_content'range loop
+--				ret(i*8+7 downto i*8) := std_logic_vector(to_unsigned(character'pos(line_content(i)), 8));
+--			end loop;
+--
+--			bin_value(0) <= ret(0);
+--			bin_value(1) <= ret(1);
+--			bin_value(2) <= ret(2);
+--			bin_value(3) <= ret(3);
+--			bin_value(4) <= ret(4);
+--			bin_value(5) <= ret(5);
+--			bin_value(6) <= ret(6);
+--			bin_value(7) <= ret(7);
+--			bin_value(8) <= ret(8);
+			
+			
+		--			for j in 1 to 8 loop		 
+		--				char := line_content(j);
+		--				if(char = '0') then
+		--					bin_value(8-j) <= '0';
+		--				elsif (char = '1') then 
+		--					bin_value(8-j) <= '1';
+		--				else 
+		--					bin_value(9) <= '1'; 
+		--				end if;	
+		--			end loop;	
+					--wait for 10 ns; --after reading each line wait for 10ns.
+			-- MyRom(i) <= bin_value;
 			i := i + 1;
 			
       end loop;
@@ -175,14 +201,14 @@ begin
 
 	--
 		-- outp(0) <= MyRom(whereToRead);
---		outp(0) <= bin_value(0);
---		outp(1) <= bin_value(1);
---		outp(2) <= bin_value(2);
---		outp(3) <= bin_value(3);
---		outp(4) <= '0';
---		outp(5) <= '0';
---		outp(6) <= '0';
---		outp(7) <= bin_value(7);
+		outp(0) <= bin_value(0);
+		outp(1) <= bin_value(1);
+		outp(2) <= bin_value(2);
+		outp(3) <= bin_value(3);
+		outp(4) <= bin_value(4);
+		outp(5) <= bin_value(5);
+		outp(6) <= bin_value(6);
+		outp(7) <= '1';
 		
 	else 
 		whereToRead <= whereToRead + 1;
